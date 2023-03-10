bcc7b5fb8dc47a9e811313917b7ed9f296c4bae6
Dmitrij Duvanov <you@example.com>
2023-03-10 20:47:39 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const a = prompt('Enter number of hours') * 3600;
+alert(a);
+
+// or prettier ->
+// const a = prompt('Enter number of hours');
+// const b = a * 3600;
+// alert(`There are ${b} seconds in ${a} hours`);
