7abb755925652bf86bfb5165e9a3477d6b811ae2
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-03-29 02:09:47 +0300

    edited

--- script/script.js
+++ script/script.js
@@ -1,29 +1,22 @@
 'use strict';
 
-function padString(str, l, sym = '', par = true) {
-    if (!str) return 'some error in arg "string"';
-    if (!l) return 'some error in arg "length"';
-    if (str.length === +l) return str;
-    if (str.length > +l) return str.substr(0, +l);
-    if (sym === '') return 'need some symbol in arg "symbol"';
-    if (par === true) {
-        let newStr = str;
-        for (let i = 0; i < l - str.length; i++) {
-            newStr = newStr + sym;
-        }
-        return newStr;
-    } else if (par === false) {
-        let newStr = str;
-        for (let i = 0; i < l - str.length; i++) {
-            newStr = sym + newStr;
-        }
-        return newStr;
-    } else {
-        return 'some error in arg "parameter"';
+const padString = (str, l, sym = '', par = true) => {
+    if (typeof str !== 'string') throw new Error('First arg should be a string');
+    if (typeof l !== 'number' || Number.isNaN(l) || !isFinite(l) ) {
+        throw new Error('Second arg should be a number');
     }
+    if (str.length === l) return str;
+    if (str.length > l) return str.substring(0, l);
+
+    if (typeof sym !== 'string' || sym.length !== 1) throw new Error ('Sym arg should be a string with 1 length');
+    if (typeof par !== 'boolean') throw new Error('par arg should be a boolean');
+
+    const symbols = sym.repeat(l - str.length);
+    return par ? str + symbols : symbols + str;
 }
 
 console.log(padString('hello', 8, '*'));
 console.log(padString('hello', 6, '*', false));
 console.log(padString('hello', 2));
+
 
