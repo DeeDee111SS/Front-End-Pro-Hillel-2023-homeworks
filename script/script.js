0254cf3ef7756550f50bf3a5fc192e310762fa3d
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-03-28 20:24:33 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const mainFunction = (callback) => {
+    const num1 = +prompt('Enter the first number', '');
+    const num2 = +prompt('Enter the second number', '');
+    if (typeof num1 !== 'number') throw new Error('num1 should be a number');
+    if (typeof num2 !== 'number') throw new Error('num2 should be a number');
+
+    return Number(callback(num1, num2));
+
+}
+
+const callbackExponentiation = (a, b) => {
+    const result = a ** b;
+    alert(result);
+}
+
+mainFunction(callbackExponentiation);
