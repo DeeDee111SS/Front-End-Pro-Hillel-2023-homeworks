202a4f2bbdb216a0e1c0f9e7f4d5bc408c8e4a8b
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-04-04 23:34:58 +0300

    first commit

--- script/script.js
+++ script/script.js
@@ -2,11 +2,29 @@
 
 // Task#1
 
+const arr1 = ['a', 'b', 'c', 'd'];
+alert(`${arr1[0]}+${arr1[1]}, ${arr1[2]}+${arr1[3]}`)
+
 // Task#2
+
+const arr2 = [2, 5, 3, 9];
+const result2 = arr2[0] * arr2[1] + arr2[2] * arr2[3];
+alert(result2);
 
 // Task#3
 
+const arr3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+alert(arr3[1][0]);
+
 // Task#4
+
+const obj = {
+    js: ['jQuery', 'Angular'],
+    php: 'hello',
+    css: 'world'
+};
+alert(obj.js[0]);
+
 
 // Task#5
 
