2207cfb4f7718e7718ba5860b84114b9d6d3d2f2
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-04-05 16:39:08 +0300

    first commit

--- script/script.js
+++ script/script.js
@@ -3,18 +3,18 @@
 // Task#1
 
 const arr1 = ['a', 'b', 'c', 'd'];
-// alert(`${arr1[0]}+${arr1[1]}, ${arr1[2]}+${arr1[3]}`)
+alert(`${arr1[0]}+${arr1[1]}, ${arr1[2]}+${arr1[3]}`)
 
 // Task#2
 
 const arr2 = [2, 5, 3, 9];
 const result2 = arr2[0] * arr2[1] + arr2[2] * arr2[3];
-// alert(result2);
+alert(result2);
 
 // Task#3
 
 const arr3 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-// alert(arr3[1][0]);
+alert(arr3[1][0]);
 
 // Task#4
 
@@ -23,7 +23,7 @@ const obj = {
     php: 'hello',
     css: 'world'
 };
-// alert(obj.js[0]);
+alert(obj.js[0]);
 
 
 // Task#5
@@ -50,7 +50,7 @@ const arrayFill = (item, index) => {
 };
 arrayFill('x', 5);
 
-// Task#8 // WIP
+// Task#8
 
 const arr7 = [1, 2, 3, 4, 5, 6, 7, 8];
 const f1 = () => {
@@ -70,7 +70,7 @@ const f1 = () => {
 }
 f1();
 
-// Task#9 // WIP
+// Task#9
 
 const arr8 = [1, 2, 3, 4, 5, 6, 7, 8];
 const f2 = () => {
@@ -79,8 +79,45 @@ const f2 = () => {
     }
 }
 f2();
-console.log(arr8);
 
-// Task#10 // WIP
+// Task#10
 
-// Task#11 // WIP
+const arr9 = [[1, 2, 3], [4, 5], [6]];
+const f3 = () => {
+    let sum1 = 0;
+    for (let i = 0; i < arr9.length; i++) {
+        if (Array.isArray(arr9[i])) {
+            for (let j = 0; j < arr9[i].length; j++) {
+                sum1 += arr9[i][j];
+            }
+        } else {
+            sum1 += arr9[i];
+        }
+    }
+    return sum1
+}
+f3();
+
+// Task#11
+
+const arr10 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+const f4 = () => {
+    let sum2 = 0;
+    for (let i = 0; i < arr10.length; i++) {
+        if (Array.isArray(arr10[i])) {
+            for (let j = 0; j < arr10[i].length; j++) { 
+                if (Array.isArray(arr10[i][j])) { 
+                    for (let k = 0; k < arr10[i][j].length; k++) {
+                        sum2 += arr10[i][j][k];
+                    }
+                } else {
+                    sum2 += arr10[i][j];
+                }
+            }
+        } else {
+            sum2 += arr10[i];
+        }
+    }
+    return sum2
+}
+f4();
