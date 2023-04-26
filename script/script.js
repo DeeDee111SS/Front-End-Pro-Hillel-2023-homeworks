65b324b60048c5f136a2975791fe298ebe20a6f8
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-04-26 21:34:05 +0300

    first commit

--- script/script.js
+++ script/script.js
@@ -5,8 +5,8 @@
         read: function () {
             this.a = +prompt('Enter the number', '');
             this.b = +prompt('Enter the number', '');
-            if (isNaN(this.a) || this.a === 0 || this.a === '') throw new Error('Invalid data');
-            if (isNaN(this.b) || this.b === 0 || this.b === '') throw new Error('Invalid data');
+            if (isNaN(this.a) || this.a === 0) throw new Error('Invalid data');
+            if (isNaN(this.b) || this.b === 0) throw new Error('Invalid data');
         },
         sum: function () {
             return this.a + this.b
