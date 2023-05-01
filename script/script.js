dd187c418192b2afb4de57c99238abd940d9cdcf
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-05-02 01:36:20 +0300

    edited

--- script/script.js
+++ script/script.js
@@ -4,113 +4,80 @@
 
 const arr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
-const positAndSum = () => {
-    const arrTemp = [];
-    for (let key in arr) {
-        if (arr[key] > 0) arrTemp.push(arr[key]);
-    };
-    const arrSum = arrTemp.reduce((sum, current) => sum + current, 0);
-    return ` sum = ${arrSum} \n number = ${arrTemp.length}`;
+const positAndSum = (arr) => {
+    const positSum = arr.reduce((sum, num) => num > 0 ? sum + num : sum, 0);
+    const positCount = arr.reduce((count, num) => num > 0 ? count + 1 : count, 0);
+    return ` sum = ${positSum} \n number = ${positCount}`;
 }
 
 // Task#2
 
-const minEl = () => {
-    let temp = 0;
-    let index = 0;
-    for (let key in arr) {
-        if (arr[key] < temp) {
-            temp = arr[key];
-            index = key;
-        }
-    }
-    return `${temp}, ${index}`;
+const minElNum = (arr) => {
+    const minEl = arr.reduce((min, num) => num < min ? num : min, 0);
+    const minElIndex = arr.indexOf(minEl);
+    return `${minEl}, ${minElIndex}`;
 }
 
 // Task#3
 
-const maxEl = () => {
-    let temp = 0;
-    let index = 0;
-    for (let key in arr) {
-        if (arr[key] > temp) {
-            temp = arr[key];
-            index = key;
-        }
-    }
-    return `${temp}, ${index}`;
-}
+const maxElNum = (arr) => {
+    const maxEl = arr.reduce((max, num) => num > max ? num : max, 0);
+    const maxElIndex = arr.indexOf(maxEl);
+    return `${maxEl}, ${maxElIndex}`;
+} 
 
 // Task#4
 
-const negatNum = () => {
-    const arrTemp = [];
-    for (let key in arr) {
-        if (arr[key] < 0) arrTemp.push(arr[key]);
-    };
-    return arrTemp.length;
+const negatNum = (arr) => {
+    const arrNegat = arr.filter((el) => el < 0);
+    return arrNegat.length;
 }
 
 // Task#5
 
-const oddPositNum = () => {
-    const arrTemp = [];
-    for (let key in arr) {
-        if (arr[key] > 0 && arr[key] % 2 !== 0) arrTemp.push(arr[key]);
-    };
-    return arrTemp.length;
+const oddPositNum = (arr) => {
+    const arrOddPosit = arr.filter((el) => el > 0 && el % 2 !== 0);
+    return arrOddPosit.length;
 }
 
 // Task#6
 
-const evenPositNum = () => {
-    const arrTemp = [];
-    for (let key in arr) {
-        if (arr[key] > 0 && arr[key] % 2 === 0) arrTemp.push(arr[key]);
-    };
-    return arrTemp.length;
+const evenPositNum = (arr) => {
+    const arrEvenPosit = arr.filter((el) => el > 0 && el % 2 === 0);
+    return arrEvenPosit.length;
 }
 
 // Task#7
 
-const evenPositSum = () => {
-    const arrTemp = [];
-    for (let key in arr) {
-        if (arr[key] > 0 && arr[key] % 2 === 0) arrTemp.push(arr[key]);
-    };
-    const arrSum = arrTemp.reduce((sum, current) => sum + current, 0);
+const evenPositSum = (arr) => {
+    const arrSum = arr.reduce((sum, el) => {
+        if (el > 0 && el % 2 === 0) return sum + el;
+        return sum;
+    }, 0);
     return arrSum;
 }
 
 // Task#8
 
-const oddPositSum = () => {
-    const arrTemp = [];
-    for (let el of arr) {
-        if (el > 0 && el % 2 !== 0) arrTemp.push(el);
-    };
-    const arrSum = arrTemp.reduce((sum, current) => sum + current, 0);
+const oddPositSum = (arr) => {
+    const arrSum = arr.reduce((sum, el) => {
+        if (el > 0 && el % 2 !== 0) return sum + el;
+        return sum;
+    }, 0);
     return arrSum;
 }
 
 // Task#9
 
-const positMult = () => {
-    const arrTemp = [];
-    for (let el of arr) {
-        if (el > 0) arrTemp.push(el);
-    };
-    const arrSum = arrTemp.reduce((sum, current) => sum * current, 1);
-    return arrSum;
+const positMult = (arr) => {
+    const arrMult = arr.reduce((mult, el) => el > 0 ? mult * el : mult, 1);
+    return arrMult;
 }
 
 // Task#10
 
-const maxOnly = () => {
-    let temp = 0;
-    for (let el of arr) {
-        if (el > temp) temp = el;           
-    }
-    const maxOnlyArr = arr.map((el) => el === temp ? el : 0);    
+const maxOnly = (arr) => {
+    const maxEl = arr.reduce((max, num) => num > max ? num : max, 0);
+    const maxOnlyArr = arr.map((el) => el === maxEl ? el : 0);    
     return maxOnlyArr;
 }
