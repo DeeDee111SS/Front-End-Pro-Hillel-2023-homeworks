97f62a4595fe9e81ba1ab7b1a0d9bbcf29e24fb6
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-05-10 12:09:36 +0300

    first commit

--- script/script.js
+++ script/script.js
@@ -2,10 +2,25 @@
 
 void function () {
 
+    const array = [1, 2, [1.1, 1.2, ['a', 'b', 'c'], 1.3], 3];
+
     const generateList = (array) => {
-        
+        const ul = document.createElement('ul');
+
+        array.map((item) => {
+            const li = document.createElement('li');
+
+            if (Array.isArray(item)) {
+                li.appendChild(generateList(item));
+            } else {
+                li.textContent = item;
+            }
+            
+            ul.appendChild(li);
+        });
+        return ul;
     }
-    console.log(generateList(array));
+
+    document.querySelector('body').appendChild(generateList(array));
     
 }();
-
