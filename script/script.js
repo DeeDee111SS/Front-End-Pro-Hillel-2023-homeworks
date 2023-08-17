e4833ccd85480e14667a065832b31978992b9889
Dmitrij Duvanov <dsduvanov@ukr.net>
2023-08-17 18:45:07 +0300

    edited

--- script/script.js
+++ script/script.js
@@ -1,35 +1,47 @@
 "use strict";
 
-void (function () {
+class GalleryAlbums {
+    queryLink = null;
+    albumPageLink = null;
+    albumList = null;
 
-    async function getAlbums() {
-        const albumList = document.getElementById("albumList");
+    constructor(selector) {
+        this.albumList = document.getElementById(selector);
+        this.queryLink = "https://jsonplaceholder.typicode.com/albums";
+        this.albumPageLink = "pages/album.html?id=";
+        this.getAlbums();
+    }
 
+    async getAlbums() {
         try {
-            const albumsResponse = await fetch(
-                "https://jsonplaceholder.typicode.com/albums"
-            );
+            const albumsResponse = await fetch(this.queryLink);
             const albums = await albumsResponse.json();
-
-            albums.forEach((album) => {
-                const albumItem = document.createElement("li");
-                albumItem.classList.add(
-                    "list-group-item",
-                    "cursor-pointer",
-                    "btn",
-                    "btn-primary"
-                );
-                albumItem.textContent = album.title;
-                albumItem.addEventListener("click", () => {
-                    window.location.href = `pages/album.html?id=${album.id}`;
-                });
-                albumList.appendChild(albumItem);
-            });
+            this.renderAlbums(albums);
         } catch (error) {
             console.error("Error fetching albums:", error);
         }
-    };
+    }
 
-    getAlbums(); 
+    renderAlbums(albums) {
+        albums.forEach((album) => {
+            const albumItem = document.createElement("li");
+            albumItem.classList.add(
+                "list-group-item",
+                "cursor-pointer",
+                "btn",
+                "btn-primary"
+            );
+            albumItem.textContent = album.title;
+            this.eventHandler(albumItem, album);
+            this.albumList.appendChild(albumItem);
+        });
+    }
 
-})();
+    eventHandler(albumitem, album) {
+        albumitem.addEventListener("click", () => {
+            window.location.href = `pages/album.html?id=${album.id}`;
+        });
+    }
+}
+
+new GalleryAlbums("albumList"); 
